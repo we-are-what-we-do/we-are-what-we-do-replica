570e666f313c48d7390297bc4a92e375ad43e2e7
tsumichara <terao.a@gmail.com>
2023-09-16 17:12:17 +0900

    Add files via upload

---
+++ src/components/Camera2.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const Camera2: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isCameraAccessible, setIsCameraAccessible] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const initCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+
+        console.log("カメラのアクセスに成功");
+        window.alert("カメラのアクセスに成功");
+        setIsCameraAccessible(true);
+      } catch (error) {
+        console.error("カメラのアクセスに失敗:", error);
+        window.alert("アプリを使用するにはカメラの許可が必要です");
+        setIsCameraAccessible(false);
+      }
+    };
+
+    initCamera();
+  }, []);
+
+  return (
+    <div>
+      <video ref={videoRef} autoPlay playsInline width="100%"></video>
+
+      {isCameraAccessible === true && <p>カメラの許可が成功しました</p>}
+      {isCameraAccessible === false && <p>アプリを使用するにはカメラの許可が必要です</p>}
+
+    </div>
+  );
+};
+
+export default Camera2;
