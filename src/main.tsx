23761e8438f997b445a8487cefa931326421930d
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-09-21 19:49:57 +0900

    revert by deleting test code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Test from './components/Test.tsx'
+import App from './App.tsx'
 import './index.css'
 import { Providers } from './redux/provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
-      <Test />
+      <App />
     </Providers>
   </React.StrictMode>,
 )
