797af3a08456a9a43c7b31d7728daa063ad31f56
MaggieIto <kaz21drama@gmail.com>
2023-10-15 00:42:39 +0900

    fix: Removed implementations that require rebuilding and redeploying if I need to increase the number of NG photos

--- src/gallery/components/PicList.tsx
+++ src/gallery/components/PicList.tsx
@@ -10,17 +10,9 @@ function PicList({updatePhotoCount}: PicListProps) {
 
   useEffect(() => {
     getPicPaths().then(async (data) => {
-      
-      // テキストファイルから不適切な画像のリストを読み込む
-      const NgPaths: string[] = await fetchNgPaths();
-
-      // 不適切な画像を排除する
-      const validData = data.filter(path => {
-        return !NgPaths.some(ngPath => path.includes(ngPath));
-      });
 
       // 下記２行は表示テスト用。４枚のサンプルデータを40枚に増やした
-      const multipliedData = validData.flatMap(pic => Array(10).fill(pic));
+      const multipliedData = data.flatMap(pic => Array(10).fill(pic));
       setPics(multipliedData);
       // setPics(validData); // 本番コード
 
@@ -44,15 +36,4 @@ function PicList({updatePhotoCount}: PicListProps) {
   );
 }
 
-// テキストファイルを読み込む関数
-async function fetchNgPaths(): Promise<string[]> {
-  const response = await fetch("../assets/ngPaths.txt");
-  if(!response.ok){
-      return [];
-  }
-  
-  const text = await response.text();
-  // テキストファイルの各行を配列として取得
-  return text.split('\n').map(line => line.trim()).filter(line => line);
-}
 export default PicList;
