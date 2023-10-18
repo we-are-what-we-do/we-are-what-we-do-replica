a9127201ade2f7966f3f88f9e0dae906c9d29058
†村神† <CA01971172@st.kawahara.ac.jp>
2023-10-18 11:49:43 +0900

    Fixed random favicon

--- src/gallery/main.tsx
+++ src/gallery/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { setRandomFavicon } from '../setRandomFavicon.ts';
-
-// ファビコンをランダムに変更する
-setRandomFavicon("./../");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/live/main.tsx
+++ src/live/main.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Providers } from './redux/provider.tsx'
-import { setRandomFavicon } from '../setRandomFavicon.ts';
-
-// ファビコンをランダムに変更する
-setRandomFavicon("./../");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
