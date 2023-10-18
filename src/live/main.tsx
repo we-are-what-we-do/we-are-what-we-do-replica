d843a254a0ebdb5863d8a5811768f52d5218dfcc
†村神† <CA01971172@st.kawahara.ac.jp>
2023-10-18 11:39:56 +0900

    Implement random favicon

--- src/live/main.tsx
+++ src/live/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Providers } from './redux/provider.tsx'
+import { setRandomFavicon } from '../setRandomFavicon.ts';
+
+// ファビコンをランダムに変更する
+setRandomFavicon("./../");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
