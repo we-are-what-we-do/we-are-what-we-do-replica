e2fc3853085f89841531baf884d34fdea036f8bd
†村神† <CA01971172@st.kawahara.ac.jp>
2023-10-23 12:16:29 +0900

    Implement gps and ip on trial-page

--- src/trial/providers/provider.tsx
+++ src/trial/providers/provider.tsx
@@ -4,24 +4,18 @@ import { DbProvider } from "./DbProvider";
 import { RingProvider } from "./RingProvider";
 import { CameraProvider } from "../../providers/CameraProvider";
 import { CaptureProvider } from "../../providers/CaptureProvider";
-import { IpProvider } from "./IpProvider";
-import { GpsProvider } from "./GpsProvider";
 
 export function Providers({ children }: {children: React.ReactNode}) {
   return (
     <Provider store={store}>
       <DbProvider>
-        <IpProvider>
-          <GpsProvider>
-            <RingProvider>{/* DbProvider, IpProvider, GpsProvider配下にRingProviderを設置 */}
-              <CameraProvider>
-                <CaptureProvider>{/* CameraProvider配下にCaptureProviderを設置 */}
-                  {children}
-                </CaptureProvider>
-              </CameraProvider>
-            </RingProvider>
-          </GpsProvider>
-        </IpProvider>
+        <RingProvider>{/* DbProvider, IpProvider, GpsProvider配下にRingProviderを設置 */}
+          <CameraProvider>
+            <CaptureProvider>{/* CameraProvider配下にCaptureProviderを設置 */}
+              {children}
+            </CaptureProvider>
+          </CameraProvider>
+        </RingProvider>
       </DbProvider>
     </Provider>
   )
