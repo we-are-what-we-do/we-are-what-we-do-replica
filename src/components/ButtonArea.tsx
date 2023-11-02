f31d4904180d05e5486d247d52f6b59add8236be
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-02 13:46:09 +0900

    fix disabled

--- src/components/ButtonArea.tsx
+++ src/components/ButtonArea.tsx
@@ -239,7 +239,7 @@ export default function ButtonArea(props: {
                 aria-label="capture-display"
                 color="primary"
                 onClick={handleTakePhotoButton}
-                disabled={!(isLoadedData && isLoadedGps)}
+                disabled={!(isLoadedData && isLoadedGps && gpsFlag)}
             >
                 <DoubleCircleIcon
                     width={ICON_SIZE}
