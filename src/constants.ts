3d56072233fd532ae15bff9f6260835dfb190d90
†村神† <107534447+CA01971172@users.noreply.github.com>
2023-11-03 12:55:02 +0900

    Update TEST_LOCATION_ID

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const API_DOMAIN: string = "api.wawwd.net";
 export const API_URL: string = `https://${API_DOMAIN}/`;
 
 // 仮で使うロケーションピンのID
-export const TEST_LOCATION_ID: string = "0cb31b0a-b6d7-4db6-9be3-c4dc51a9047f";
+export const TEST_LOCATION_ID: string = "b7d8074d-746d-43d4-86d0-0d142fe157ef";
 
 // アプリケーションサーバーのwebsocketのURL
 export const WS_URL: string = `wss://${API_DOMAIN}/ws-rings?location=${TEST_LOCATION_ID}`;
