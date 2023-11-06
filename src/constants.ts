9d7196716b89a69dee74239e7d811248001bf7d1
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-06 09:16:30 +0900

    change API_domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,13 @@
 // アプリケーションサーバーのドメイン
-export const API_DOMAIN: string = "api.wawwd.net";
-// export const API_DOMAIN: string = "testApi.wawwd.net";
+// export const API_DOMAIN: string = "api.wawwd.net";
+export const API_DOMAIN: string = "testApi.wawwd.net";
 
 // アプリケーションサーバーのURL
 export const API_URL: string = `https://${API_DOMAIN}/`;
 
 // 仮で使うロケーションピンのID
-export const TEST_LOCATION_ID: string = "b7d8074d-746d-43d4-86d0-0d142fe157ef";
+// export const TEST_LOCATION_ID: string = "b7d8074d-746d-43d4-86d0-0d142fe157ef";
+export const TEST_LOCATION_ID: string = "f8777292-0a66-4762-94cc-5243cd8bfb3e";
 
 // アプリケーションサーバーのwebsocketのURL
 export const WS_URL: string = `wss://${API_DOMAIN}/ws-rings?location=${TEST_LOCATION_ID}`;
