6a88d50e88a22e2e5b2499b2a891d80b51754b41
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-06 10:49:24 +0900

    Implement remove localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Providers } from './redux/provider.tsx'
 
+// URLクエリが適切なら、localStorageを初期化する
+const searchParams = new URLSearchParams(window.location.search);
+const deleteTarget: string | null = searchParams.get("removeItem");
+if(deleteTarget) localStorage.removeItem(deleteTarget);
+
+// ReactをDomに追加する
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
