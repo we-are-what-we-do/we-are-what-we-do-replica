beaffbd87f3b2caf2e57af8af77214e1cb581740
†村神† <CA01971172@st.kawahara.ac.jp>
2023-11-06 09:19:17 +0900

    fix TEST_LOCATION_ID

--- src/constants.ts
+++ src/constants.ts
@@ -6,8 +6,7 @@ export const API_DOMAIN: string = "testApi.wawwd.net";
 export const API_URL: string = `https://${API_DOMAIN}/`;
 
 // 仮で使うロケーションピンのID
-// export const TEST_LOCATION_ID: string = "b7d8074d-746d-43d4-86d0-0d142fe157ef";
-export const TEST_LOCATION_ID: string = "f8777292-0a66-4762-94cc-5243cd8bfb3e";
+export const TEST_LOCATION_ID: string = "b7d8074d-746d-43d4-86d0-0d142fe157ef";
 
 // アプリケーションサーバーのwebsocketのURL
 export const WS_URL: string = `wss://${API_DOMAIN}/ws-rings?location=${TEST_LOCATION_ID}`;
